4feccc91a735d61b1c4e61ad1808e3680cba2ead
Igor Raskatov <aura.star@yandex.ru>
2022-12-04 15:02:15 +0300

    Add husky test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
+import { useEffect } from 'react';
 
-export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
+export default function App({
+	Component,
+	pageProps,
+	router,
+}: AppProps): JSX.Element {
+	useEffect(() => {
+		router.events.on('routeChangeComplete', (url: string) => {
+			if (typeof window !== undefined) {
+				ym('hit', url);
+			}
+		});
+	}, [router.events]);
 	return (
 		<>
 			<Head>
@@ -12,13 +26,15 @@ export default function App({ Component, pageProps, router }: AppProps): JSX.Ele
 					property="og:url"
 					content={`${process.env.NEXT_PUBLIC_DOMAIN}${router.asPath}`}
 				/>
-				<meta
-					property="og:locale"
-					content='ru_RU'
-				/>
+				<meta property="og:locale" content="ru_RU" />
 				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://mc.yandex.ru" />
 			</Head>
-
+			<YMInitializer
+				accounts={[]}
+				options={{ webvisor: true, defer: true }}
+				version="2"
+			/>
 			<Component {...pageProps} />
 		</>
 	);
